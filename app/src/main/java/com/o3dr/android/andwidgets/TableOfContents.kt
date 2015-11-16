42b28784397be01caf6a49e2c437bc24e8843fbe
ne0fhyk <fhuyakou@gmail.com>
2015-11-15 22:01:45 -0800

    project cleanup. adding sample app to visualize the library's widgets and style.

---
+++ app/src/main/java/com/o3dr/android/andwidgets/TableOfContents.kt
@@ -0,0 +1,9 @@
+package com.o3dr.android.andwidgets
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Fredia Huya-Kouadio on 11/15/15.
+ */
+class TableOfContents : AppCompatActivity() {
+}
